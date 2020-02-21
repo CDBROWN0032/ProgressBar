c190cf93e158322a466051a058748e4a8c864440
Christopher Brown <riv4r@live.com>
2020-02-20 20:41:22 -0500

    base progress counter complete

--- ProgressBar/Program.cs
+++ ProgressBar/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace ProgressBar
 {
@@ -10,6 +7,44 @@ namespace ProgressBar
     {
         static void Main(string[] args)
         {
+            WelcomeMessage();
+
+            PercentComplete("Uploading Files...", 4, true);                            
         }
+
+
+        static void PercentComplete(string message, int total, bool test = false)
+        {
+            int percentComplete;
+            int currentIndex;
+            var location = "Place";
+            var errorCount = 1;
+
+            for (int i = 0; i <= total; i++)
+            {
+                currentIndex = i > 0 ? i : 1;
+                percentComplete = ((100 * (i)) / total);
+                Console.Write($"\r{message} {percentComplete}% complete");
+                if (currentIndex == total)
+                {
+                    Console.WriteLine(Environment.NewLine);
+                    Console.WriteLine($"Files Uploaded to {location} with {errorCount} error(s).");
+
+                }
+
+                if(test)
+                    Thread.Sleep(100);
+            }
+            
+            Console.ReadKey();
+        }
+
+        static void WelcomeMessage()
+        {
+            Console.WriteLine("CMS File Uploader v1.0");
+            Console.WriteLine("Commands:");
+            Console.WriteLine("");
+        }
+
     }
 }
